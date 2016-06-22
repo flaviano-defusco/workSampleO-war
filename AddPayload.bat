a19ae1c23c5e000fc2778e61fccf151b31e8d1c2
Flaviano de Fusco <F.DeFusco@iaea.org>
2016-06-22 16:59:54 +0200

    second payload + repo config + compile tag

---
+++ AddPayload.bat
@@ -0,0 +1,19 @@
+git add config.groovy
+
+git add build.gradle
+
+git add gradle.properties
+
+git add RunBuild.bat
+
+git add InitializeGit.bat
+
+git add src
+
+git commit -m "adding payload"
+pause
+
+git push -u origin master
+pause
+
+pause
---
+++ AddPayloadTwo.bat
@@ -0,0 +1,12 @@
+git add AddPayload.bat
+
+git add AddPayloadTwo.bat
+pause
+
+git commit -m "second payload + repo config + compile tag"
+pause
+
+git push -u origin master
+pause
+
+pause
